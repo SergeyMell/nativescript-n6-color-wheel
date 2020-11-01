05a50d50d57a66cf90dbe00ed515f24ca7b97f5a
Sergey Mell <sergey.mell@agilie.com>
2020-11-01 15:29:20 +0200

    Fixed issue with initial color position determination

--- src/index.ios.ts
+++ src/index.ios.ts
@@ -1,9 +1,10 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 
 import * as common from './common';
-import { ColorWheel as ColorWheelDefinition } from '.';
-import { Color } from 'tns-core-modules/color';
-import { releaseNativeObject } from '@nativescript/core/utils/utils';
+import {ColorWheel as ColorWheelDefinition} from '.';
+import {Color} from 'tns-core-modules/color';
+import {releaseNativeObject} from '@nativescript/core/utils/utils';
+
 global.moduleMerge(common, exports);
 
 /**
@@ -13,190 +14,193 @@ global.moduleMerge(common, exports);
 // @NativeClass
 class TapHandler extends NSObject {
 
-  public tap(args) {
-    /** Define tap position */
-    const position = args.locationInView(args.view);
-    const x = position.x;
-    const y = position.y;
+    public tap(args) {
+        /** Define tap position */
+        const position = args.locationInView(args.view);
+        const x = position.x;
+        const y = position.y;
 
-    /** Draw 1 pixel canvas in place */
-    const pixel = malloc(4 * interop.sizeof(interop.types.uint8));
-    const colorSpace = CGColorSpaceCreateDeviceRGB();
-    const context =
-      CGBitmapContextCreate(pixel, 1, 1, 8, 4,
-        colorSpace, CGImageAlphaInfo.kCGImageAlphaPremultipliedFirst);
-    CGContextTranslateCTM(context, -x, -y);
-    args.view.layer.renderInContext(context);
+        /** Draw 1 pixel canvas in place */
+        const pixel = malloc(4 * interop.sizeof(interop.types.uint8));
+        const colorSpace = CGColorSpaceCreateDeviceRGB();
+        const context =
+            CGBitmapContextCreate(pixel, 1, 1, 8, 4,
+                colorSpace, CGImageAlphaInfo.kCGImageAlphaPremultipliedFirst);
+        CGContextTranslateCTM(context, -x, -y);
+        args.view.layer.renderInContext(context);
 
 
-    /** Emit selected color */
+        /** Emit selected color */
 
-    const reference = new interop.Reference(interop.types.uint8, pixel);
-    const owner = (<any>args.view).owner;
+        const reference = new interop.Reference(interop.types.uint8, pixel);
+        const owner = (<any>args.view).owner;
 
-    if (reference.value && owner) {
-      owner.notify({
-        eventName: 'colorSelect',
-        object: Object.assign({}, owner, {
-          isFirstChange: false,
-          color: new Color(reference[0], reference[1], reference[2], reference[3]),
-          colorPosition: {x, y}
-        })
-      });
+        if (reference.value && owner) {
+            owner.notify({
+                eventName: 'colorSelect',
+                object: Object.assign({}, owner, {
+                    isFirstChange: false,
+                    color: new Color(reference[0], reference[1], reference[2], reference[3]),
+                    colorPosition: {x, y}
+                })
+            });
+        }
+
+        /** Release allocated memory */
+        releaseNativeObject(colorSpace);
+        releaseNativeObject(context);
+        free(pixel);
     }
 
-    /** Release allocated memory */
-    releaseNativeObject(colorSpace);
-    releaseNativeObject(context);
-    free(pixel);
-  }
-
-  public static ObjCExposedMethods = {
-    'tap': { returns: interop.types.void, params: [interop.types.id, interop.types.id] }
-  };
+    public static ObjCExposedMethods = {
+        'tap': {returns: interop.types.void, params: [interop.types.id, interop.types.id]}
+    };
 }
 
 const handler = new TapHandler();
 
 export class ColorWheel extends common.ColorWheelCommon implements ColorWheelDefinition {
 
-  nativeView: UIImageView;
+    nativeView: UIImageView;
 
-  /** Creates new image view. */
-  public createNativeView(): Object {
-    const imageView = UIImageView.new();
+    /** Creates new image view. */
+    public createNativeView(): Object {
+        const imageView = UIImageView.new();
 
-    const tap = new UITapGestureRecognizer({
-      target: handler,
-      action: 'tap'
-    });
+        const tap = new UITapGestureRecognizer({
+            target: handler,
+            action: 'tap'
+        });
 
-    imageView.addGestureRecognizer(tap);
-    imageView.userInteractionEnabled = true;
+        imageView.addGestureRecognizer(tap);
+        imageView.userInteractionEnabled = true;
+
+        /**
+         * Generating color wheel by means of **CIFilter**
+         * [Post with details]{@link https://noahgilmore.com/blog/cifilter-colorwheel}
+         */
+        const colorSpace = CGColorSpaceCreateDeviceRGB();
+        const filter = CIFilter.filterWithNameWithInputParameters('CIHueSaturationValueGradient', {
+            //@ts-ignore
+            'inputColorSpace': colorSpace,
+            'inputDither': 0,
+            'inputRadius': this.radius,
+            'inputSoftness': 0,
+            'inputValue': 1
+        });
+
+        const image = UIImage.imageWithCIImage(filter.outputImage);
+        imageView.image = image;
+
+        releaseNativeObject(colorSpace);
+
+        return imageView;
+    }
 
     /**
-     * Generating color wheel by means of **CIFilter**
-     * [Post with details]{@link https://noahgilmore.com/blog/cifilter-colorwheel}
+     * Initializes properties/listeners of the native view.
      */
-    const colorSpace = CGColorSpaceCreateDeviceRGB();
-    const filter = CIFilter.filterWithNameWithInputParameters('CIHueSaturationValueGradient', {
-      //@ts-ignore
-      'inputColorSpace': colorSpace,
-      'inputDither': 0,
-      'inputRadius': this.radius,
-      'inputSoftness': 0,
-      'inputValue': 1
-    });
+    initNativeView(): void {
+        /**
+         * Attach the owner to nativeView.
+         * When nativeView is tapped we get the owning JS object through this field.
+         */
+        (<any>this.nativeView).owner = this;
+        super.initNativeView();
+    }
 
-    const image = UIImage.imageWithCIImage(filter.outputImage);
-    imageView.image = image;
 
-    releaseNativeObject(colorSpace);
-
-    return imageView;
-  }
-
-  /**
-   * Initializes properties/listeners of the native view.
-   */
-  initNativeView(): void {
     /**
-     * Attach the owner to nativeView.
-     * When nativeView is tapped we get the owning JS object through this field.
+     * Clean up references to the native view and resets nativeView to its original state.
+     * If you have changed nativeView in some other way except through setNative callbacks
+     * you have a chance here to revert it back to its original state
+     * so that it could be reused later.
      */
-    (<any>this.nativeView).owner = this;
-    super.initNativeView();
-  }
+    disposeNativeView(): void {
+        // Remove reference from native listener to this instance.
+        (<any>this.nativeView).owner = null;
 
+        // If you want to recycle nativeView and have modified the nativeView
+        // without using Property or CssProperty (e.g. outside our property system - 'setNative' callbacks)
+        // you have to reset it to its initial state here.
+        super.disposeNativeView();
+    }
 
-  /**
-   * Clean up references to the native view and resets nativeView to its original state.
-   * If you have changed nativeView in some other way except through setNative callbacks
-   * you have a chance here to revert it back to its original state
-   * so that it could be reused later.
-   */
-  disposeNativeView(): void {
-    // Remove reference from native listener to this instance.
-    (<any>this.nativeView).owner = null;
+    /**
+     * TODO: This is an algorithm of full range. It's definitely not optimal
+     *       should think about something better
+     * Determination of point coordinates by color is done according to
+     * {@link https://stackoverflow.com/questions/18170398/how-to-get-location-of-a-specified-color-in-an-uiimage}
+     * {@link https://github.com/erica/iOS-6-Advanced-Cookbook/blob/master/C06%20-%20Images/04%20-%20Rotation%20Accelerate/UIImage-Utils.m}
+     */
+    [common.colorProperty.setNative](value: string | Color) {
+        const color = value instanceof Color ? value : new Color(value);
 
-    // If you want to recycle nativeView and have modified the nativeView
-    // without using Property or CssProperty (e.g. outside our property system - 'setNative' callbacks)
-    // you have to reset it to its initial state here.
-    super.disposeNativeView();
-  }
+        const width = this.nativeView.image.size.width;
+        const height = this.nativeView.image.size.height;
 
-  /**
-   * TODO: This is an algorithm of full range. It's definitely not optimal
-   *       should think about something better
-   * Determination of point coordinates by color is done according to
-   * {@link https://stackoverflow.com/questions/18170398/how-to-get-location-of-a-specified-color-in-an-uiimage}
-   * {@link https://github.com/erica/iOS-6-Advanced-Cookbook/blob/master/C06%20-%20Images/04%20-%20Rotation%20Accelerate/UIImage-Utils.m}
-   */
-  [common.colorProperty.setNative](value: string | Color) {
-    const color = value instanceof Color ? value : new Color(value);
+        let colorSpace = CGColorSpaceCreateDeviceRGB();
+        let bitmapData = malloc(4 * width * height);
+        let context =
+            CGBitmapContextCreate(bitmapData, width, height, 8, width * 4,
+                colorSpace, CGImageAlphaInfo.kCGImageAlphaPremultipliedFirst);
 
-    const width = this.nativeView.image.size.width;
-    const height = this.nativeView.image.size.height;
+        const rect = CGRectMake(0, 0, width, height);
+        const cgImage = CIContext.new().createCGImageFromRect(this.nativeView.image.CIImage, rect);
 
-    let colorSpace = CGColorSpaceCreateDeviceRGB();
-    let bitmapData = malloc(4 * width * height);
-    let context =
-      CGBitmapContextCreate(bitmapData, width, height, 8, width * 4,
-        colorSpace, CGImageAlphaInfo.kCGImageAlphaPremultipliedFirst);
+        CGContextDrawImage(context, rect, cgImage);
 
-    const rect = CGRectMake(0, 0, width, height);
-    const cgImage = CIContext.new().createCGImageFromRect(this.nativeView.image.CIImage, rect);
+        const reference = new interop.Reference(interop.types.uint8, bitmapData);
 
-    CGContextDrawImage(context, rect, cgImage);
-
-    const reference = new interop.Reference(interop.types.uint8, bitmapData);
-
-    let resX = -1;
-    let resY = -1;
-    let metrics = Infinity;
-    for (let x = 0; x < width; x++) {
-      for (let y = 0; y < height; y++) {
-        const red = reference[redOffset(x, y, width)];
-        const green = reference[greenOffset(x, y, width)];
-        const blue = reference[blueOffset(x, y, width)];
-        const distance = Math.abs(red - color.r) +
-          Math.abs(green - color.g) +
-          Math.abs(blue - color.b);
-        if (distance < metrics) {
-          metrics = distance;
-          resX = x;
-          resY = y;
+        let resX = -1;
+        let resY = -1;
+        let metrics = Infinity;
+        for (let x = 0; x < width; x++) {
+            for (let y = 0; y < height; y++) {
+                if ((x - this.radius) * (x - this.radius) + (y - this.radius) * (y - this.radius) >= this.radius * this.radius) {
+                    continue;
+                }
+                const red = reference[redOffset(x, y, width)];
+                const green = reference[greenOffset(x, y, width)];
+                const blue = reference[blueOffset(x, y, width)];
+                const distance = Math.abs(red - color.r) +
+                    Math.abs(green - color.g) +
+                    Math.abs(blue - color.b);
+                if (distance < metrics) {
+                    metrics = distance;
+                    resX = x;
+                    resY = y;
+                }
+            }
         }
-      }
-    }
-    /** Release allocated memory */
-    releaseNativeObject(colorSpace);
-    releaseNativeObject(context);
-    free(bitmapData);
+        /** Release allocated memory */
+        releaseNativeObject(colorSpace);
+        releaseNativeObject(context);
+        free(bitmapData);
 
-    if (resX > -1 && resY > -1) {
-      this.notify({
-        eventName: 'colorSelect',
-        object: Object.assign({}, this, {
-          isFirstChange: true,
-          color,
-          colorPosition: { x: resX, y: resY }
-        })
-      });
-    }
+        if (resX > -1 && resY > -1) {
+            this.notify({
+                eventName: 'colorSelect',
+                object: Object.assign({}, this, {
+                    isFirstChange: true,
+                    color,
+                    colorPosition: {x: resX, y: resY}
+                })
+            });
+        }
 
-  }
+    }
 
 }
 
 export function redOffset(x, y, w) {
-  return y * w * 4 + x * 4 + 1;
+    return y * w * 4 + x * 4 + 1;
 }
 
 export function greenOffset(x, y, w) {
-  return y * w * 4 + x * 4 + 2;
+    return y * w * 4 + x * 4 + 2;
 }
 
 export function blueOffset(x, y, w) {
-  return y * w * 4 + x * 4 + 3;
+    return y * w * 4 + x * 4 + 3;
 }
